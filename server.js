7743f20aa40a42d5929e1243e230147409e22d1f
Stephen <campbell@Stephens-iMac.local>
2026-03-24 09:49:36 -0300

    Fix route order so root serves longform.html

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'longform.html'));
 });
+
+app.use(express.static(path.join(__dirname)));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Joyride landing pages running');
